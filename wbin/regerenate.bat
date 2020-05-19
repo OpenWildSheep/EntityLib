83d89381b5b98900c2dd17ddb19abcc7683e202d
lhamot <loic.hamot@yahoo.fr>
2020-05-19 16:55:11 +0200

    Add the wbin schema with c++ and python generated files

---
+++ wbin/regerenate.bat
@@ -0,0 +1,3 @@
+"../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable wbin.fbs
+"../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable wbin.fbs
+pause

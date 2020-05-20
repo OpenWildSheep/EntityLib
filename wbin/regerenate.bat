0fb16080c29ea9a2a361fbe631a0397edf09a1df
lhamot <loic.hamot@yahoo.fr>
2020-05-20 16:02:07 +0200

    Add the wskl

--- wbin/regerenate.bat
+++ wbin/regerenate.bat
@@ -4,4 +4,6 @@
 "../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable who.fbs
 "../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable wviz.fbs
 "../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable wviz.fbs
+"../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable wskl.fbs
+"../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable wskl.fbs
 pause

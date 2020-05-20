96a96319992460092d57dfcfa93ad59af7d776c4
lhamot <loic.hamot@yahoo.fr>
2020-05-20 15:44:29 +0200

    Add wviz schema

--- wbin/regerenate.bat
+++ wbin/regerenate.bat
@@ -2,4 +2,6 @@
 "../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable wbin.fbs
 "../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable who.fbs
 "../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable who.fbs
+"../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable wviz.fbs
+"../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable wviz.fbs
 pause

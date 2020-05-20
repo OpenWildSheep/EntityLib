f5e459d87869dc69f71b0d3543c4aa7f36c84a96
lhamot <loic.hamot@yahoo.fr>
2020-05-20 15:03:33 +0200

    Add the HeightObj definition

--- wbin/regerenate.bat
+++ wbin/regerenate.bat
@@ -1,3 +1,5 @@
 "../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable wbin.fbs
 "../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable wbin.fbs
+"../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable who.fbs
+"../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable who.fbs
 pause

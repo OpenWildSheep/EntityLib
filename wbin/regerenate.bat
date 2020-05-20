5db22d7a04f717f92205a1470e0817a1cf99872b
lhamot <loic.hamot@yahoo.fr>
2020-05-20 17:46:14 +0200

    Fix some typos

--- wbin/regerenate.bat
+++ wbin/regerenate.bat
@@ -2,8 +2,8 @@
 "../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable wbin.fbs
 "../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable who.fbs
 "../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable who.fbs
-"../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable wviz.fbs
-"../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable wviz.fbs
+"../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable wvis.fbs
+"../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable wvis.fbs
 "../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable wskl.fbs
 "../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable wskl.fbs
 "../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable wphys.fbs

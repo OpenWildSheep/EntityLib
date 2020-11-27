bca77324ca8f89c7f4682109fd668fba28fe2ccb
sebastienandary <sebastien.andary@wildsheepstudio.com>
2020-11-27 15:53:44 +0100

    [EntityLib] fix cpp tests

--- EntityLib/Test/TestEntityDLL.cpp
+++ EntityLib/Test/TestEntityDLL.cpp
@@ -326,12 +326,21 @@ try
         entlib.saveEntity(*ent, "prefab.copy.entity");
 
         // TEST SubScene detach
-        auto&& originalSubEntities =  ent->getSubSceneComponent()->embedded->getObjects();
+        const auto& subEntities = ent->getSubSceneComponent()->embedded->getObjects();
+        std::vector<Ent::Entity*> originalSubEntities;
+		std::transform(begin(subEntities), end(subEntities), 
+			std::back_inserter(originalSubEntities), 
+			[](auto&& entityPtr){ return entityPtr.get(); });
+        ENTLIB_ASSERT(not originalSubEntities.empty());
         auto detachedSubScene = ent->getSubSceneComponent()->detachEmbedded();
         ENTLIB_ASSERT(ent->getSubSceneComponent()->embedded->getObjects().empty());
+        std::vector<Ent::Entity*> detachedSubEntities;
+		std::transform(begin(detachedSubScene->getObjects()), end(detachedSubScene->getObjects()), 
+			std::back_inserter(detachedSubEntities), 
+			[](auto&& entityPtr){ return entityPtr.get(); });
         ENTLIB_ASSERT(std::equal(
             begin(originalSubEntities), end(originalSubEntities),
-            begin(detachedSubScene->getObjects())));
+            begin(detachedSubEntities)));
     }
     {
         // Test the readOnly prefab.entity

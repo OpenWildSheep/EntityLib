2b238a15437c37d55e5150feda7576291daef52c
Fabien Delpiano <fabien@pastagames.net>
2020-12-10 16:16:03 +0100

    [PR] Initialising as "no rotation" quaternion.

--- WildRPCClient/WildRPCClient/include/Parameters.h
+++ WildRPCClient/WildRPCClient/include/Parameters.h
@@ -31,7 +31,7 @@ namespace WRPC
 
 	struct Quat
 	{
-		float x = 0.0f, y = 0.0f, z = 0.0f, w = 0.0f;
+		float x = 0.0f, y = 0.0f, z = 0.0f, w = 1.0f;
 		Quat(){}
 		Quat(float _x, float _y, float _z, float _w) : x(_x), y(_y), z(_z), w(_w) {}
 	};

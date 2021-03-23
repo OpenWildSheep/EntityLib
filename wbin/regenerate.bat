442ef1efc2d4cfbbe928dc90994a3d90dadbe423
Christophe Blondel <christophe.blondel@wildsheepstudio.com>
2021-03-23 16:00:05 +0100

    Voxel volume format

--- wbin/regenerate.bat
+++ wbin/regenerate.bat
@@ -16,8 +16,6 @@
 "../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable wsp.fbs
 "../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable wtso.fbs
 "../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable wtso.fbs
-"../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable wgpcloud.fbs
-"../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable wgpcloud.fbs
 "../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable wgvoxelvolume.fbs
 "../../External/FlatBuffers/Release/flatc" --python --gen-object-api --gen-mutable wgvoxelvolume.fbs
 "../../External/FlatBuffers/Release/flatc" --cpp --gen-object-api --gen-mutable wstampcloud.fbs

8ca4242e0720f7b1c27e05af5d98ffa8a9e7d6a8
Benoit Fouletier <benoit.fouletier@gmail.com>
2021-04-13 13:12:04 +0200

    WildRPC: move generated files to per-language subdirectories

--- WiLDRPC/regenerate.bat
+++ WiLDRPC/regenerate.bat
@@ -2,19 +2,25 @@
 set flatc="../../External/FlatBuffers/Release/flatc"
 ::set flatc="D:/WildCode/FlatBuffers/flatbuffers/out/build/x64-Debug/flatc"
 
+:: delete previous outputs, in case we removed some schemas
+rmdir /s/q cpp\WiLDRPC
+rmdir /s/q python\WildRPC
+
 :: generate all the .fbs schemas in the directory
 for %%f in (*.fbs) do (
 	::echo %%~ff
-	%flatc% --cpp    %%~ff
-	%flatc% --python %%~ff
+	%flatc% --cpp    -o .\cpp\WiLDRPC %%~ff
+	%flatc% --python -o .\python\     %%~ff
 )
 
 :: replace __init__.py with the list of all generated types
 (
-	for %%f in (WildRPC\*.py) do (
+	for %%f in (python\WildRPC\*.py) do (
 		if not "%%~nf" == "__init__" (
 		if not "%%~nf" == "__all__" (
 			echo from . import %%~nf
 		))
 	)
-) > WildRPC\__all__.py
+) > python\WildRPC\__all__.py
+
+pause

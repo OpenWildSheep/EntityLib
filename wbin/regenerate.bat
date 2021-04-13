a1e4b46453efb30df95ade4c4929d4e19c1f7ed9
Benoit Fouletier <benoit.fouletier@gmail.com>
2021-04-13 14:01:03 +0200

    wbin: move generated files to per-language subdirectories

--- wbin/regenerate.bat
+++ wbin/regenerate.bat
@@ -2,21 +2,25 @@
 set flatc="../../External/FlatBuffers/Release/flatc"
 ::set flatc="D:/WildCode/FlatBuffers/flatbuffers/out/build/x64-Debug/flatc"
 
+:: delete previous outputs, in case we removed some schemas
+rmdir /s/q cpp
+rmdir /s/q python
+
 :: generate all the .fbs schemas in the directory
 for %%f in (*.fbs) do (
 	::echo %%~ff
-	%flatc% --cpp    --gen-object-api --gen-mutable %%~ff
-	%flatc% --python --gen-object-api --gen-mutable %%~ff
+	%flatc% --cpp    --gen-object-api --gen-mutable -o .\cpp\wbin %%~ff
+	%flatc% --python --gen-object-api --gen-mutable -o .\python\  %%~ff
 )
 
 :: replace __init__.py with the list of all generated types
 (
-	for %%f in (WBIN\*.py) do (
+	for %%f in (python\WBIN\*.py) do (
 		if not "%%~nf" == "__init__" (
 		if not "%%~nf" == "__all__" (
 			echo from . import %%~nf
 		))
 	)
-) > WBIN\__all__.py
+) > python\WBIN\__all__.py
 
 pause

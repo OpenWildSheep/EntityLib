c42de81d0a8540085b3fe092fb65b282648cc64d
Benoit Fouletier <benoit.fouletier@gmail.com>
2021-05-28 12:39:01 +0200

    RPC: add explicitly-typed overloads, for languages that don't support binding to template methods (eg Wolf, Python)

--- WildRPCClient/WildRPCClient/include/Parameters.h
+++ WildRPCClient/WildRPCClient/include/Parameters.h
@@ -183,21 +183,30 @@ namespace WRPC
             m_type = _type;
         }
 
+    private:
+        // NOTE: these private constructors take a dummy second parameter, to disambiguate the explicitly-typed constructors
         template <typename T>
-        Parameter(const T& _scalar)
+        Parameter(const T& _scalar, char)
         {
             m_value = vector<T>{_scalar};
             m_type = TypeInfo(ContainerType_Scalar, GetElementType<T>());
         }
 
         template <typename T>
-        Parameter(const vector<T>& _vector)
+        Parameter(const vector<T>& _vector, char)
         {
             m_value = _vector;
             m_type = TypeInfo(ContainerType_Array, GetElementType<T>());
         }
 
-        Parameter(const char* _scalar) : Parameter(std::string(_scalar)) { }
+    public:
+        template <typename T>
+        Parameter(const T& _scalar) : Parameter(_scalar, 0) { }
+
+        template <typename T>
+        Parameter(const vector<T>& _vector) : Parameter(_vector, 0) { }
+
+        Parameter(const char* _scalar) : Parameter(string(_scalar)) { }
 
 
         /// Get the value as a scalar.
@@ -250,6 +259,85 @@ namespace WRPC
 
         bool EncodeIn(uint8_t* _buffer, const size_t _bufferSize, uoffset_t& _offset) const;
         bool DecodeFrom(uint8_t* _buffer, const size_t _bufferSize, uoffset_t& _offset);
+
+
+        // explicitly-typed overloads, for languages that don't support binding to template methods (eg Wolf, Python)
+        Parameter(const bool& _scalar) : Parameter(_scalar, 0) { }
+        Parameter(const int& _scalar) : Parameter(_scalar, 0) { }
+        Parameter(const float& _scalar) : Parameter(_scalar, 0) { }
+        Parameter(const Vector2& _scalar) : Parameter(_scalar, 0) { }
+        Parameter(const Vector3i& _scalar) : Parameter(_scalar, 0) { }
+        Parameter(const Vector3& _scalar) : Parameter(_scalar, 0) { }
+        Parameter(const Quat& _scalar) : Parameter(_scalar, 0) { }
+        Parameter(const Color& _scalar) : Parameter(_scalar, 0) { }
+        Parameter(const Position& _scalar) : Parameter(_scalar, 0) { }
+        Parameter(const string& _scalar) : Parameter(_scalar, 0) { }
+
+        Parameter(const vector<bool>& _vector) : Parameter(_vector, 0) { }
+        Parameter(const vector<int>& _vector) : Parameter(_vector, 0) { }
+        Parameter(const vector<float>& _vector) : Parameter(_vector, 0) { }
+        Parameter(const vector<Vector2>& _vector) : Parameter(_vector, 0) { }
+        Parameter(const vector<Vector3i>& _vector) : Parameter(_vector, 0) { }
+        Parameter(const vector<Vector3>& _vector) : Parameter(_vector, 0) { }
+        Parameter(const vector<Quat>& _vector) : Parameter(_vector, 0) { }
+        Parameter(const vector<Color>& _vector) : Parameter(_vector, 0) { }
+        Parameter(const vector<Position>& _vector) : Parameter(_vector, 0) { }
+        Parameter(const vector<string>& _vector) : Parameter(_vector, 0) { }
+
+        Parameter(const std::initializer_list<bool>& _list) : Parameter(vector<bool>(_list), 0) { }
+        Parameter(const std::initializer_list<int>& _list) : Parameter(vector<int>(_list), 0) { }
+        Parameter(const std::initializer_list<float>& _list) : Parameter(vector<float>(_list), 0) { }
+        Parameter(const std::initializer_list<Vector2>& _list) : Parameter(vector<Vector2>(_list), 0) { }
+        Parameter(const std::initializer_list<Vector3i>& _list) : Parameter(vector<Vector3i>(_list), 0) { }
+        Parameter(const std::initializer_list<Vector3>& _list) : Parameter(vector<Vector3>(_list), 0) { }
+        Parameter(const std::initializer_list<Quat>& _list) : Parameter(vector<Quat>(_list), 0) { }
+        Parameter(const std::initializer_list<Color>& _list) : Parameter(vector<Color>(_list), 0) { }
+        Parameter(const std::initializer_list<Position>& _list) : Parameter(vector<Position>(_list), 0) { }
+        Parameter(const std::initializer_list<string>& _list) : Parameter(vector<string>(_list), 0) { }
+
+        bool AsScalar_bool() const { return AsScalar<bool>(); }
+        int AsScalar_int() const { return AsScalar<int>(); }
+        float AsScalar_float() const { return AsScalar<float>(); }
+        Vector2 AsScalar_Vector2() const { return AsScalar<Vector2>(); }
+        Vector3i AsScalar_Vector3i() const { return AsScalar<Vector3i>(); }
+        Vector3 AsScalar_Vector3() const { return AsScalar<Vector3>(); }
+        Quat AsScalar_Quat() const { return AsScalar<Quat>(); }
+        Color AsScalar_Color() const { return AsScalar<Color>(); }
+        Position AsScalar_Position() const { return AsScalar<Position>(); }
+        string AsScalar_string() const { return AsScalar<string>(); }
+
+        const vector<bool>& AsVector_bool() const { return AsVector<bool>(); }
+        const vector<int>& AsVector_int() const { return AsVector<int>(); }
+        const vector<float>& AsVector_float() const { return AsVector<float>(); }
+        const vector<Vector2>& AsVector_Vector2() const { return AsVector<Vector2>(); }
+        const vector<Vector3i>& AsVector_Vector3i() const { return AsVector<Vector3i>(); }
+        const vector<Vector3>& AsVector_Vector3() const { return AsVector<Vector3>(); }
+        const vector<Quat>& AsVector_Quat() const { return AsVector<Quat>(); }
+        const vector<Color>& AsVector_Color() const { return AsVector<Color>(); }
+        const vector<Position>& AsVector_Position() const { return AsVector<Position>(); }
+        const vector<string>& AsVector_string() const { return AsVector<string>(); }
+
+        bool TryGetScalar_bool(bool& _value) const { return TryGetScalar<bool>(_value); }
+        bool TryGetScalar_int(int& _value) const { return TryGetScalar<int>(_value); }
+        bool TryGetScalar_float(float& _value) const { return TryGetScalar<float>(_value); }
+        bool TryGetScalar_Vector2(Vector2& _value) const { return TryGetScalar<Vector2>(_value); }
+        bool TryGetScalar_Vector3i(Vector3i& _value) const { return TryGetScalar<Vector3i>(_value); }
+        bool TryGetScalar_Vector3(Vector3& _value) const { return TryGetScalar<Vector3>(_value); }
+        bool TryGetScalar_Quat(Quat& _value) const { return TryGetScalar<Quat>(_value); }
+        bool TryGetScalar_Color(Color& _value) const { return TryGetScalar<Color>(_value); }
+        bool TryGetScalar_Position(Position& _value) const { return TryGetScalar<Position>(_value); }
+        bool TryGetScalar_string(string& _value) const { return TryGetScalar<string>(_value); }
+
+        bool TryGetVector_bool(vector<bool>& _value) const { return TryGetVector<bool>(_value); }
+        bool TryGetVector_int(vector<int>& _value) const { return TryGetVector<int>(_value); }
+        bool TryGetVector_float(vector<float>& _value) const { return TryGetVector<float>(_value); }
+        bool TryGetVector_Vector2(vector<Vector2>& _value) const { return TryGetVector<Vector2>(_value); }
+        bool TryGetVector_Vector3i(vector<Vector3i>& _value) const { return TryGetVector<Vector3i>(_value); }
+        bool TryGetVector_Vector3(vector<Vector3>& _value) const { return TryGetVector<Vector3>(_value); }
+        bool TryGetVector_Quat(vector<Quat>& _value) const { return TryGetVector<Quat>(_value); }
+        bool TryGetVector_Color(vector<Color>& _value) const { return TryGetVector<Color>(_value); }
+        bool TryGetVector_Position(vector<Position>& _value) const { return TryGetVector<Position>(_value); }
+        bool TryGetVector_string(vector<string>& _value) const { return TryGetVector<string>(_value); }
     };
 
     // -----------------------
@@ -305,6 +393,31 @@ namespace WRPC
             m_index++;
             return value;
         }
+
+        // explicitly-typed overloads, for languages that don't support binding to template methods (eg Wolf, Python)
+        typedef Parameter::string string;
+
+        bool NextScalar_bool() { return NextScalar<bool>(); }
+        int NextScalar_int() { return NextScalar<int>(); }
+        float NextScalar_float() { return NextScalar<float>(); }
+        Vector2 NextScalar_Vector2() { return NextScalar<Vector2>(); }
+        Vector3i NextScalar_Vector3i() { return NextScalar<Vector3i>(); }
+        Vector3 NextScalar_Vector3() { return NextScalar<Vector3>(); }
+        Quat NextScalar_Quat() { return NextScalar<Quat>(); }
+        Color NextScalar_Color() { return NextScalar<Color>(); }
+        Position NextScalar_Position() { return NextScalar<Position>(); }
+        string NextScalar_string() { return NextScalar<string>(); }
+
+        const Parameter::vector<bool>& NextVector_bool() { return NextVector<bool>(); }
+        const Parameter::vector<int>& NextVector_int() { return NextVector<int>(); }
+        const Parameter::vector<float>& NextVector_float() { return NextVector<float>(); }
+        const Parameter::vector<Vector2>& NextVector_Vector2() { return NextVector<Vector2>(); }
+        const Parameter::vector<Vector3i>& NextVector_Vector3i() { return NextVector<Vector3i>(); }
+        const Parameter::vector<Vector3>& NextVector_Vector3() { return NextVector<Vector3>(); }
+        const Parameter::vector<Quat>& NextVector_Quat() { return NextVector<Quat>(); }
+        const Parameter::vector<Color>& NextVector_Color() { return NextVector<Color>(); }
+        const Parameter::vector<Position>& NextVector_Position() { return NextVector<Position>(); }
+        const Parameter::vector<string>& NextVector_string() { return NextVector<string>(); }
 
     private:
         RPC_Error m_error;

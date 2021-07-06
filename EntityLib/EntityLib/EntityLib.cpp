fb4a73788676b7c45aa17d75262b0b7c3209f1ce
Egazel <elie.gazel@wildsheepstudio.com>
2021-07-06 14:30:44 +0200

    Hack : Don't save the prefab's name when applying to prefab

--- EntityLib/EntityLib/EntityLib.cpp
+++ EntityLib/EntityLib/EntityLib.cpp
@@ -3293,8 +3293,10 @@ void Ent::Entity::applyToPrefab()
         throw ContextException("This entity has no prefab");
     }
     auto prefab = entlib->loadEntity(prefabPath);
+    auto prefabName = prefab->getName();
     // When the value is overridden is the source, we want to make it overridden in the dest => CopyOverride
     applyAllValuesButPrefab(*prefab, CopyMode::CopyOverride);
+    prefab->setName(prefabName);
     resetInstanceOf(prefabPath); // Reset 'this' to a vanilla instance of prefab
     entlib->saveEntity(*prefab, prefabPath);
 }

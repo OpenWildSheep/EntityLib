17d5ceeea8413acc1c25ff1bf75c903ed242a766
lhamot <loic.hamot@yahoo.fr>
2021-08-31 11:37:06 +0200

    Remove __call__ functions in entgen_helpers.py (no more useful)

--- EntityLib/EntLibAPIGenerator/py/entgen_helpers.py
+++ EntityLib/EntLibAPIGenerator/py/entgen_helpers.py
@@ -23,11 +23,8 @@ class Base(object):
 
 class Primitive(Base, Generic[T]):
     def __init__(self, item_type, node = None):  # type: (Type[T], EntityLibPy.Node) -> None
-        super().__init__(node)
+        super(Primitive, self).__init__(node)
         self._item_type = item_type
-
-    def __call__(self, node):  # type: (EntityLibPy.Node) -> Primitive[T]
-        return Primitive(self._item_type, node)
 
     @property
     def value(self):  # type: () -> T
@@ -49,45 +46,40 @@ class Primitive(Base, Generic[T]):
 
 class Float(Primitive[float]):
     def __init__(self, node):
-        super().__init__(float, node)
-    def __call__(self, node):  # type: (EntityLibPy.Node) -> Float
-        return Float(node)
+        super(Float, self).__init__(float, node)
+
     def set(self, val):  # type: (float) -> None
         return self._node.set_float(val)
 
 
 class Int(Primitive[int]):
     def __init__(self, node):
-        super().__init__(int, node)
-    def __call__(self, node):  # type: (EntityLibPy.Node) -> Int
-        return Int(node)
+        super(Int, self).__init__(int, node)
+
     def set(self, val):  # type: (int) -> None
         return self._node.set_int(val)
 
 
 class Bool(Primitive[bool]):
     def __init__(self, node):
-        super().__init__(bool, node)
-    def __call__(self, node):  # type: (EntityLibPy.Node) -> Bool
-        return Bool(node)
+        super(Bool, self).__init__(bool, node)
+
     def set(self, val):  # type: (bool) -> None
         return self._node.set_bool(val)
 
 
 class String(Primitive[str]):
     def __init__(self, node):
-        super().__init__(str, node)
-    def __call__(self, node):  # type: (EntityLibPy.Node) -> String
-        return String(node)
+        super(String, self).__init__(str, node)
+
     def set(self, val):  # type: (str) -> None
         return self._node.set_string(val)
 
 
 class EntityRef(Primitive[EntityLibPy.EntityRef]):
     def __init__(self, node):
-        super().__init__(EntityLibPy.EntityRef, node)
-    def __call__(self, node):  # type: (EntityLibPy.Node) -> EntityRef
-        return EntityRef(node)
+        super(EntityRef, self).__init__(EntityLibPy.EntityRef, node)
+
     def set(self, val):  # type: (EntityLibPy.EntityRef) -> None
         return self._node.set_entityref(val)
 
@@ -116,11 +108,8 @@ class Union(Base):
 
 class Array(Base, Generic[T]):
     def __init__(self, item_ctor, node = None):  # type: (Callable[[Any], T], EntityLibPy.Node) -> None
-        super().__init__(node)
+        super(Array, self).__init__(node)
         self._item_ctor = item_ctor
-
-    def __call__(self, node):  # type: (...) -> ArrayClass[T]
-       return  ArrayClass(self._item_ctor, node)
 
     def __getitem__(self, idx: int):
         return self._item_ctor(self._node.at(idx))
@@ -130,15 +119,15 @@ class Array(Base, Generic[T]):
 
 
 class PrimArray(Array[T], Generic[T]):
-    def set(self, values):  # type: ([T]) -> None
+    def set(self, values):  # type: (List[T]) -> None
         for i in range(self._node.size()):
             self._node.at(i).value = values[i]
 
     @property
-    def value(self):  # type: (...) -> [T]
+    def value(self):  # type: (...) -> List[T]
         return [self._node.at(i).value for i in range(self._node.size())]
     @value.setter
-    def value(self, values):  # type: (...) -> [T]
+    def value(self, values):  # type: (List[T]) -> None
         self.set(values)
 
     def __setitem__(self, idx, value):  # type: (int, ...) -> None
@@ -147,11 +136,8 @@ class PrimArray(Array[T], Generic[T]):
 
 class ObjectSet(Base, Generic[T]):
     def __init__(self, item_ctor, node = None): # type: (Callable[[Any], T], EntityLibPy.Node) -> None
-        super().__init__(node)
+        super(ObjectSet, self).__init__(node)
         self._item_ctor = item_ctor
-
-    def __call__(self, node):  # type: (...) -> ObjectSetClass[T]
-       return  ObjectSet(self._item_ctor, node)
 
     def get(self, key): # type: (...) -> T
         return self._item_ctor(self._node.map_get(key))
@@ -174,12 +160,9 @@ V = TypeVar("V")
 
 class Map(Base, Generic[K, V]):
     def __init__(self, key_type, item_ctor, node = None):  # type: (Type[K], Callable[[Any], V], EntityLibPy.Node) -> None
-        super().__init__(node)
+        super(Map, self).__init__(node)
         self._key_type = key_type
         self._item_ctor = item_ctor
-
-    def __call__(self, node):       # type: (...) -> MapClass[K, V]
-       return  Map(self._item_ctor, node)
 
     def to_internal(self, key):  # type : (T) -> T
         if issubclass(type(key), Enum):
@@ -206,11 +189,8 @@ class Map(Base, Generic[K, V]):
 
 class PrimitiveSet(Base, Generic[T]):
     def __init__(self, item_ctor, node = None):  # type: (Callable[[Any], T], EntityLibPy.Node) -> None
-        super().__init__(node)
+        super(PrimitiveSet, self).__init__(node)
         self._item_ctor = item_ctor
-
-    def __call__(self, node):  # type: (...) -> PrimitiveSetClass[T]
-       return  PrimitiveSet(self._item_ctor, node)
 
     def to_internal(self, key):  # type : (T) -> T
         if issubclass(type(key), Enum):
@@ -241,12 +221,9 @@ class PrimitiveSet(Base, Generic[T]):
 TTuple = TypeVar("TTuple", bound=Tuple)
 class TupleNode(Base, Generic[TTuple]):
     def __init__(self, typelist, node = None): # type: (TTuple, EntityLibPy.Node) -> None
-        super().__init__(node)
+        super(TupleNode, self).__init__(node)
         self.typelist = typelist
-    """
-    def __call__(self, node):  # type: (EntityLibPy.Node) -> TupleClass[TTuple]
-       return  TupleClass(self.typelist, node)
-    """
+
     def __getitem__(self, key):
         return self.typelist[key](self._node.at(key))
 

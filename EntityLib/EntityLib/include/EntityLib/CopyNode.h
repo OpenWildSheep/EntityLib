325df53e2e16b718d38c7caa83e0aecaf404205e
lhamot <loic.hamot@yahoo.fr>
2022-01-26 09:55:10 +0100

    Enhance CopyToEmptyNode. Better naming. Copy "InstanceOf". Fix inUnionSetElement when empty Data.

--- EntityLib/EntityLib/include/EntityLib/CopyNode.h
+++ EntityLib/EntityLib/include/EntityLib/CopyNode.h
@@ -7,124 +7,132 @@ namespace Ent
     /// Deep copy from a Node to an empty Node
     class CopyToEmptyNode : public RecursiveVisitor
     {
-        Cursor& source;
-        Cursor& dest;
+        Cursor& m_source;
+        Cursor& m_dest;
 
     public:
         CopyToEmptyNode(Cursor& _source, Cursor& _dest)
-            : source(_source)
-            , dest(_dest)
+            : m_source(_source)
+            , m_dest(_dest)
         {
+        }
+        void inObject() override
+        {
+            m_dest.setInstanceOf(m_source.getInstanceOf());
         }
         bool inObjectField(char const* _key) override
         {
-            dest.enterObjectField(_key);
+            m_dest.enterObjectField(_key);
             return true;
         }
         void outObjectField([[maybe_unused]] char const* _key) override
         {
-            dest.exit();
+            m_dest.exit();
         }
         void inUnion(char const* _type) override
         {
-            if (source.isSet())
+            if (m_source.isSet())
             {
-                dest.setUnionType(_type);
+                m_dest.setUnionType(_type);
             }
-            dest.enterUnionData(_type);
+            m_dest.enterUnionData(_type);
         }
         void outUnion() override
         {
-            dest.exit();
+            m_dest.exit();
         }
         void inMapElement(char const* _key) override
         {
-            dest.enterMapItem(_key);
+            m_dest.enterMapItem(_key);
         }
         void inMapElement(int64_t _key) override
         {
-            dest.enterMapItem(_key);
+            m_dest.enterMapItem(_key);
         }
         void outMapElement() override
         {
-            dest.exit();
+            m_dest.exit();
         }
         void inArrayElement(size_t _index) override
         {
-            dest.enterArrayItem(_index);
+            m_dest.enterArrayItem(_index);
         }
         void outArrayElement() override
         {
-            dest.exit();
+            m_dest.exit();
         }
         void key(char const* _key) override
         {
-            dest.insertPrimSetKey(_key);
+            m_dest.insertPrimSetKey(_key);
         }
         void key(int64_t _key) override
         {
-            dest.insertPrimSetKey(_key);
+            m_dest.insertPrimSetKey(_key);
         }
         void inUnionSetElement(char const* _type) override
         {
-            dest.enterUnionSetItem(_type);
+            m_dest.enterUnionSetItem(_type);
+            if (m_source.isSet())
+            {
+                m_dest.buildPath(); // Force it to set
+            }
         }
         void outUnionSetElement() override
         {
-            dest.exit();
+            m_dest.exit();
         }
         void inObjectSetElement(char const* _key) override
         {
-            dest.enterObjectSetItem(_key);
+            m_dest.enterObjectSetItem(_key);
         }
         void inObjectSetElement(int64_t _key) override
         {
-            dest.enterObjectSetItem(_key);
+            m_dest.enterObjectSetItem(_key);
         }
         void outObjectSetElement() override
         {
-            dest.exit();
+            m_dest.exit();
         }
         void inArray() override
         {
-            if (source.isSet())
+            if (m_source.isSet())
             {
-                dest.setSize(source.size());
+                m_dest.setSize(m_source.size());
             }
         }
         void boolNode() override
         {
-            if (source.isSet())
+            if (m_source.isSet())
             {
-                dest.setBool(source.getBool());
+                m_dest.setBool(m_source.getBool());
             }
         }
         void intNode() override
         {
-            if (source.isSet())
+            if (m_source.isSet())
             {
-                dest.setInt(source.getInt());
+                m_dest.setInt(m_source.getInt());
             }
         }
         void floatNode() override
         {
-            if (source.isSet())
+            if (m_source.isSet())
             {
-                dest.setFloat(source.getFloat());
+                m_dest.setFloat(m_source.getFloat());
             }
         }
         void stringNode() override
         {
-            if (source.isSet())
+            if (m_source.isSet())
             {
-                dest.setString(source.getString());
+                m_dest.setString(m_source.getString());
             }
         }
         void entityRefNode() override
         {
-            if (source.isSet())
+            if (m_source.isSet())
             {
-                dest.setEntityRef(source.getEntityRef());
+                m_dest.setEntityRef(m_source.getEntityRef());
             }
         }
     };

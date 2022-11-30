479a83a567afae754d411e42a4a5003c4e6fe718
Loïc HAMOT <loic.hamot@yahoo.fr>
2022-11-30 19:33:58 +0100

    Fix EntityLib::saveJsonFile when saving relative path... (#439)
    * Fix EntityLib::saveJsonFile when saving relative path, and RawData is not the working_dir.
    
    * Fix the case of an absolute _filepath

--- EntityLib/EntityLib/EntityLib.cpp
+++ EntityLib/EntityLib/EntityLib.cpp
@@ -636,18 +636,19 @@ namespace Ent
     void EntityLib::saveJsonFile(json const* doc, char const* _filepath, char const* _schema) const
     {
         std::filesystem::path const filepath = very_weakly_canonical(_filepath);
-        auto const tempFilename = rawdataPath / (filepath.string() + ".tmp");
+        auto const absFilename = filepath.is_relative() ? rawdataPath / filepath : filepath;
+        auto const tempFilename = std::filesystem::path(absFilename.string() + ".tmp");
         {
             std::ofstream ofs(tempFilename);
             if (not ofs.is_open())
             {
-                throw ContextException("Can't open %s for write", filepath.string().c_str());
+                throw ContextException("Can't open %s for write", tempFilename.string().c_str());
             }
             json copy = *doc;
             copy["$schema"] = format(schemaFormat, _schema);
             ofs << copy.dump(4);
         }
-        try3Times([&] { rename(tempFilename, filepath); });
+        try3Times([&] { rename(tempFilename, absFilename); });
         if (m_jsonDatabase.count(filepath) == 0)
         {
             m_jsonDatabase[filepath] = std::make_unique<VersionedJson>();

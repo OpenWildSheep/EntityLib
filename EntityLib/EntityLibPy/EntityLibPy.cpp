742ddb468fb05960326f9866aea553eb2f8fd591
lhamot <loic.hamot@wildsheepstudio.com>
2023-04-18 15:12:11 +0200

    Display the filename and noderef, when there is an error. Especially with wrong enum values

--- EntityLib/EntityLibPy/EntityLibPy.cpp
+++ EntityLib/EntityLibPy/EntityLibPy.cpp
@@ -864,6 +864,7 @@ PYBIND11_MODULE(EntityLibPy, ent)
         .def("push_back", &Property::pushBack)
         .def("pop_back", &Property::popBack)
         .def("apply_to_prefab", &Property::applyToPrefab)
+        .def_property_readonly("debug_string", &Property::getDebugString)
         .def("search_child", &searchChild)
         .def_static("from_ptr", [](intptr_t _ptr) {return (Property*)_ptr;}, py::return_value_policy::reference_internal)
         ;

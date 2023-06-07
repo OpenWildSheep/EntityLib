ef21ca20532038b92dc849028b839fe748117de8
lhamot <loic.hamot@wildsheepstudio.com>
2023-06-07 12:10:53 +0200

    Enhance README.md

--- regen_entgen.sh
+++ regen_entgen.sh
@@ -5,23 +5,23 @@ echo P4_ROOT is $p4_root
 echo Build EntityLib
 vs16path="C:/Program Files (x86)/SCE/Common/SceVSI-VS16/bin/vs16build.exe"
 
-if ! "$vs16path" EntityLib/EntityLib2019.sln //build "Release|x64" //project EntityLib/UpdateSchemas/UpdateSchemas2019.vcxproj //sn-dbs; then
+if ! "$vs16path" EntityLib2019.sln //build "Release|x64" //project UpdateSchemas/UpdateSchemas2019.vcxproj //sn-dbs; then
 	echo Build UpdateSchemas2019 Failed
 	exit 1
 fi
-if ! "$vs16path" EntityLib/EntityLib2019.sln //build "Release|x64" //project EntityLib/EntLibAPIGenerator/EntLibAPIGenerator.vcxproj //sn-dbs; then
+if ! "$vs16path" EntityLib2019.sln //build "Release|x64" //project EntLibAPIGenerator/EntLibAPIGenerator.vcxproj //sn-dbs; then
 	echo Build EntLibAPIGenerator Failed
 	exit 1
 fi
 
 echo Generate the EntGen API
-if ! EntityLib/x64/Release/EntLibAPIGenerator.exe EntityLib/EntLibAPIGenerator/resources EntityLib/EntGen "$p4_root"; then
+if ! x64/Release/EntLibAPIGenerator.exe EntLibAPIGenerator/resources EntGen "$p4_root"; then
 	echo EntLibAPIGenerator Failed
 	exit 1
 fi
 
 echo Merge Runtime schemas with edition schemas into MergesComponents.json
-if ! EntityLib/x64/Release/UpdateSchemas.exe $p4_root/Tools; then
+if ! x64/Release/UpdateSchemas.exe $p4_root/Tools; then
 	echo UpdateSchemas Failed
 	exit 1
 fi

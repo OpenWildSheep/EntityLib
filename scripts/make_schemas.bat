d63a55cc8cfad7b952569ee3b86a24efddb9bea1
lhamot <loic.hamot@wildsheepstudio.com>
2023-06-28 11:07:44 +0200

    Add unit tests for demo

--- scripts/make_schemas.bat
+++ scripts/make_schemas.bat
@@ -1,3 +1,2 @@
-cd ../build
-"Release/UpdateSchemas.exe" -o Schemas -e ../DemoSchemas/EditionComponents.json,../DemoSchemas/Scene-schema.json -r ../DemoSchemas/RuntimeComponents.json
+"../build/Release/UpdateSchemas.exe" -o ../build/Schemas -e ../DemoSchemas/EditionComponents.json,../DemoSchemas/Scene-schema.json -r ../DemoSchemas/RuntimeComponents.json
 pause

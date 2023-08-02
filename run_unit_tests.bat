8483cd5678eb26fa07c5f8f66e477b210f4b44a6
lhamot <loic.hamot@yahoo.fr>
2023-08-02 18:16:34 +0200

    Enhance the README.md about how to build

--- run_unit_tests.bat
+++ run_unit_tests.bat
@@ -1,7 +1,7 @@
 call build.bat
 
-build\Release\UpdateSchemas -o build/Schemas -e DemoSchemas/EditionComponents.json,DemoSchemas/Scene-schema.json -r DemoSchemas/RuntimeComponents.json
-build\Release\EntLibAPIGenerator -s build/Schemas -d build/EntGen -r EntLibAPIGenerator/resources
+build/Release/UpdateSchemas -o build/Schemas -e DemoSchemas/EditionComponents.json,DemoSchemas/Scene-schema.json -r DemoSchemas/RuntimeComponents.json
+build/Release/EntLibAPIGenerator -s build/Schemas -d build/EntGen -r EntLibAPIGenerator/resources
 cmake --build build --config Release --target TestEntityDLL
 cd Test
-..\build\Release\TestEntityDLL
+../build/Release/TestEntityDLL
